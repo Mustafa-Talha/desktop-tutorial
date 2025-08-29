df74fd3a4bb3b0f8fcb0e7509a6ee9c9f5146848
Mustafa-Talha <talhaa709euro@gmail.com>
2025-08-29 15:19:08 +0300

    Add files via upload

---
+++ ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CSharpWebChatbot.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
